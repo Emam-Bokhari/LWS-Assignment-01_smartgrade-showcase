0ade680f9a5a64fc06e9fa867469ca9bd2f92847
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-02-29 11:06:37 +0600

    add Footer component in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { Fragment } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import StudentsTable from "./components/StudentsTable/StudentsTable";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <Fragment>
+
       <div className="bg-[#172227] font-[Inter] text-white" >
+
         <Navbar />
+
         <HeroSection />
+
         <StudentsTable />
+
+        <Footer />
+
       </div>
+
     </Fragment>
   )
 }
